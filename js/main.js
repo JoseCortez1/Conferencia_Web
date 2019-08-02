b2ffcdbe968a0e8dfb03fd0463fe46ff0d1cba19
JoseCortez1 <jose.cucei96@gmail.com>
2019-08-02 16:34:33 -0500

    se agregan los cambios para poder mostrar el contador

--- js/main.js
+++ js/main.js
@@ -183,4 +183,24 @@ $(function(){
         $(enlace).fadeIn(1000);
         return false;
     });
+
+
+    /**Animaciones para los numeros */
+
+    $('.resumen-evento div:nth-child(1) p.numero').animateNumber({number: 6},3000);
+    $('.resumen-evento div:nth-child(2) p.numero').animateNumber({number: 15},1200);
+    $('.resumen-evento div:nth-child(3) p.numero').animateNumber({number: 3}, 6000);
+    $('.resumen-evento div:nth-child(4) p.numero').animateNumber({number: 9}, 2000);
+
+
+    /**Contador de tiempo */
+
+    $('.cuenta-regresiva').countdown('2019/08/12  07:00:00',function(event){
+        $('#dias').html(event.strftime('%D'));
+        $('#horas').html(event.strftime('%H'));
+        $('#minutos').html(event.strftime('%M'));
+        $('#segundos').html(event.strftime('%S'));
+    });
+
+
 });
