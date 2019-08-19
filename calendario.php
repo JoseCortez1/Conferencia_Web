f695592f0a95054cb23a013bc3b1b9fd84fbf36c
zebraUbuntu <jose.cucei96@gmail.com>
2019-08-19 15:43:41 -0500

    se agrega estructura para mostrar la informacion de los eventos, se modifica la consula a al bd principal para poder obtener los iconos de las categorias

--- calendario.php
+++ calendario.php
@@ -11,7 +11,7 @@
       try {
         require_once('includes/funciones/conexion.php');
 
-        $sql = " SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+        $sql = " SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
         $sql .= " FROM eventos ";
         $sql .= " INNER JOIN categoria_evento ";
         $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
@@ -44,6 +44,7 @@
               'fecha' => $eventos['fecha_evento'],
               'hora' => $eventos['hora_evento'],
               'categoria' => $eventos['cat_evento'],
+              'icono' => 'fas ' . $eventos['icono'],
               'nombre' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
 
             );
@@ -67,15 +68,27 @@
               echo strftime("%A, %d de %B del %Y", strtotime($dia) );
             ?>
           </h3>
+          <?php foreach($lista_eventos as $evento){ ?>
+              <div class="dia">
+                <p class="titulo"> <?php echo $evento["titulo"]; ?> </p>
+                <p class="hora">
+                  <i class="fas fa-clock" aria-hidden="true"></i>
+                  <?php echo $evento["fecha"] . " " . $evento['hora']; ?>
+                </p>
+                <p>
+                <i class="<?php echo $evento['icono']; ?>" aria-hidden="true"></i>
+                  <?php echo $evento["categoria"]; ?>
+                </p>
+                <p>
+                  <i class="fas fa-user" aria-hidden="true"></i>
+                  <?php echo $evento['nombre'];?>
+                </p>
 
-      <?php } ?>
+              </div>
 
+          <?php } //Fin foreach eventos ?>
 
-      <pre style="text-align: left">
-        <?php var_dump($calendario); ?>
-      </pre>
-
-    </div>
+      <?php } //Fin foreach Dia?>
 
     <?php
 
