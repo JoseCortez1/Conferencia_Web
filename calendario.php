831f00cfde059e8b4b7ca55113acde1d566792e1
zebraUbuntu <jose.cucei96@gmail.com>
2019-08-19 08:14:21 -0500

    Se crea funcion para añadir informacion de la base de datos para la parte del calendario y se empieza a crear funcion para mostrarlo en el sitio, queda incompleto en la parte de mostrar datos de los eventos 'Solo mustra la fecha'

--- calendario.php
+++ calendario.php
@@ -6,6 +6,83 @@
 
   <section class="seccion contenedor">
     <h2>Calendario Invitados </h2>
+    <?php
+
+      try {
+        require_once('includes/funciones/conexion.php');
+
+        $sql = " SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+        $sql .= " FROM eventos ";
+        $sql .= " INNER JOIN categoria_evento ";
+        $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+        $sql .= " INNER JOIN invitados ";
+        $sql .= " ON eventos.id_invitado = invitados.id_invitado ";
+        $sql .= " ORDER BY fecha_evento";
+
+
+
+        $resultado = $conn->query($sql);
+
+      } catch (\Exception $e) {
+        echo $e->getMessage();
+      }
+
+
+    ?>
+    <div class="calendario">
+      <?php
+
+          $calendario = array();
+
+          while($eventos = $resultado->fetch_assoc()){
+
+
+            $fecha = $eventos['fecha_evento'];  //Obtiene la fecha del evento de tal manera que luego podemos ponerla en el arreglo $calendario para agruparlos por fecha
+
+            $evento = array(
+              'titulo' => $eventos['nombre_evento'],
+              'fecha' => $eventos['fecha_evento'],
+              'hora' => $eventos['hora_evento'],
+              'categoria' => $eventos['cat_evento'],
+              'nombre' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
+
+            );
+
+            $calendario[$fecha][] = $evento;
+
+      ?>
+
+      <?php } ?>
+
+      <?php
+
+        //Recorriendo el arreglo
+
+        foreach($calendario as $dia => $lista_eventos){?>
+          <h3>
+            <i class="fa fa-calendar"></i>
+            <?php
+              setlocale( LC_TIME, 'es_ES.UTF-8');
+
+              echo strftime("%A, %d de %B del %Y", strtotime($dia) );
+            ?>
+          </h3>
+
+      <?php } ?>
+
+
+      <pre style="text-align: left">
+        <?php var_dump($calendario); ?>
+      </pre>
+
+    </div>
+
+    <?php
+
+      $conn->close();
+
+    ?>
+
   </section>
 <?php
 
